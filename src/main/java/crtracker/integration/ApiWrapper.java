235b157e0fc2dc0a2ae1057d35c58cd5423020a4
mlieshoff <michaellieshoff@gmx.de>
2020-11-16 08:19:52 +0100

    removed string allocation.

--- src/main/java/crtracker/integration/ApiWrapper.java
+++ src/main/java/crtracker/integration/ApiWrapper.java
@@ -4,11 +4,9 @@ import crtracker.integration.clashroyale.OfficialApi;
 import jcrapi2.response.GetClanResponse;
 import jcrapi2.response.GetCurrentClanRiverRaceResponse;
 import jcrapi2.response.GetPlayerBattleLogResponse;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
 public class ApiWrapper {
 
   private final String proxyApiUrl;
@@ -16,33 +14,45 @@ public class ApiWrapper {
   private final String officialApiUrl;
   private final String officialApiToken;
 
+  private final OfficialApi officialApi;
+  private final OfficialApi proxiedApi;
+
+  public ApiWrapper(String proxyApiUrl, String proxyApiToken, String officialApiUrl, String officialApiToken) {
+    this.proxyApiUrl = proxyApiUrl;
+    this.proxyApiToken = proxyApiToken;
+    this.officialApiUrl = officialApiUrl;
+    this.officialApiToken = officialApiToken;
+    proxiedApi = new OfficialApi(proxyApiUrl, proxyApiToken);
+    officialApi = new OfficialApi(officialApiUrl, officialApiToken);
+  }
+
   public GetClanResponse getClanData(String clanTag) {
     try {
       log.info("try get clan data from official api...");
-      return new OfficialApi(proxyApiUrl, proxyApiToken).getClanData(clanTag);
+      return proxiedApi.getClanData(clanTag);
     } catch (Exception e) {
       log.error("catched exception while getting clan data[{}]...", officialApiUrl, e);
-      return new OfficialApi(officialApiUrl, officialApiToken).getClanData(clanTag);
+      return officialApi.getClanData(clanTag);
     }
   }
 
   public GetPlayerBattleLogResponse getBattleLogFor(String playerTag) {
     try {
       log.info("try get battle log data from official api...");
-      return new OfficialApi(proxyApiUrl, proxyApiToken).getPlayerBattleLogData(playerTag);
+      return proxiedApi.getPlayerBattleLogData(playerTag);
     } catch (Exception e) {
       log.error("catched exception while getting battle log [{}]...", officialApiToken, e);
-      return new OfficialApi(officialApiUrl, officialApiToken).getPlayerBattleLogData(playerTag);
+      return officialApi.getPlayerBattleLogData(playerTag);
     }
   }
 
   public GetCurrentClanRiverRaceResponse getCurrentClanRiverRace(String clanTag) {
     try {
       log.info("try get battle log data from official api...");
-      return new OfficialApi(proxyApiUrl, proxyApiToken).getCurrentClanRiverRace(clanTag);
+      return proxiedApi.getCurrentClanRiverRace(clanTag);
     } catch (Exception e) {
       log.error("catched exception while getting battle log [{}]...", officialApiToken, e);
-      return new OfficialApi(officialApiUrl, officialApiToken).getCurrentClanRiverRace(clanTag);
+      return officialApi.getCurrentClanRiverRace(clanTag);
     }
   }
 
--- src/main/java/crtracker/integration/discord/DiscordMessageService.java
+++ src/main/java/crtracker/integration/discord/DiscordMessageService.java
@@ -20,9 +20,16 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class DiscordMessageService {
 
-  private static final String EMPTY = "\u200b";
-  private static final String VERSUS_TEXT = "versus";
-  private static final String VERSUS_PATTERN = "%s:%s";
+  private static final String ONE_VS_ONE_VERSUS_TEXT = "1 vs 1";
+  private static final String TWO_VS_TWO_VERSUS_TEXT = "2 vs 2";
+  private static final String VERSUS_TEXT = "Versus";
+  private static final String VERSUS_PATTERN = "%s:%s %s";
+  private static final String AND_TEXT_PATTERN = "%s and %s";
+  private static final String VERSUS_TEXT_PATTERN = "%s versus %s";
+  private static final String GAMEMODE_PATTERN = "%s (%s)";
+  private static final String DRAW_TEXT = "Unentschieden";
+  private static final String WON_TEXT = "Sieg";
+  private static final String LOST_TEXT = "Niederlage";
   private static final String URL_BATTLE_PATTERN = "https://statsroyale.com/de/profile/%s/battles";
   private static final String AUTHOR = "RoyalCardForces";
   private static final String URL_CLAN = "https://statsroyale.com/de/clan/RP88QQG";
@@ -30,6 +37,7 @@ public class DiscordMessageService {
   private static final String
       THUMBNAIL =
       "https://cdn.royaleapi.com/static/img/branding/royaleapi-logo.png?t=d36aee7acba48c1f894a438c6823cd01a7d2f4e9";
+  private static final String LINK_PATTERN = "[%s](https://royaleapi.com/player/%s)";
 
   private final ConcurrentHashMap<String, DiscordApi> cache = new ConcurrentHashMap<>();
 
@@ -80,37 +88,47 @@ public class DiscordMessageService {
 
     if (team.size() > 1) {
       embedBuilder
-          .addField("2 vs 2", transformPlayer(team.get(0)) + " and " + transformPlayer(team.get(1)),
-              false);
+          .addField(
+              TWO_VS_TWO_VERSUS_TEXT,
+              format(AND_TEXT_PATTERN, transformPlayer(team.get(0)), transformPlayer(team.get(1))),
+              false
+          );
       embedBuilder
-          .addField("Versus", transformPlayer(opponentTeam.get(0)) + " and " + transformPlayer(opponentTeam.get(1)),
-              false);
+          .addField(
+              VERSUS_TEXT,
+              format(AND_TEXT_PATTERN, transformPlayer(opponentTeam.get(0)), transformPlayer(opponentTeam.get(1))),
+              false
+          );
     } else {
-      embedBuilder.addField("1 vs 1",
-          transformPlayer(team.get(0)) + " versus " + transformPlayer(opponentTeam.get(0)), false);
+      embedBuilder.addField(
+          ONE_VS_ONE_VERSUS_TEXT,
+          format(VERSUS_TEXT_PATTERN, transformPlayer(team.get(0)), transformPlayer(opponentTeam.get(0))),
+          false
+      );
     }
 
     int playerCrowns = playerBattleLog.getTeam().get(0).getCrowns();
     int opponentCrowns = playerBattleLog.getOpponent().get(0).getCrowns();
-    String msg = "Unentschieden";
+    String msg = DRAW_TEXT;
     if (playerCrowns > opponentCrowns) {
-      msg = "Sieg";
+      msg = WON_TEXT;
     } else if (playerCrowns < opponentCrowns) {
-      msg = "Niederlage";
+      msg = LOST_TEXT;
     }
-    embedBuilder.addField(playerBattleLog.getArena().getName(),
-        format(VERSUS_PATTERN, playerCrowns, opponentCrowns) + " " + msg, false);
+    embedBuilder.addField(
+        playerBattleLog.getArena().getName(),
+        format(VERSUS_PATTERN, playerCrowns, opponentCrowns, msg),
+        false
+    );
     return embedBuilder.build();
   }
 
   private String transformPlayer(PlayerBattleLogTeam playerBattleLogTeam) {
-    return "[" + playerBattleLogTeam.getName() + "](https://royaleapi.com/player/" + playerBattleLogTeam.getTag()
-        .substring(1) + ")";
+    return format(LINK_PATTERN, playerBattleLogTeam.getName(), playerBattleLogTeam.getTag().substring(1));
   }
 
   private String transformPlayer(PlayerBattleLogOpponent playerBattleLogOpponent) {
-    return "[" + playerBattleLogOpponent.getName() + "](https://royaleapi.com/player/" + playerBattleLogOpponent
-        .getTag().substring(1) + ")";
+    return format(LINK_PATTERN, playerBattleLogOpponent.getName(), playerBattleLogOpponent.getTag().substring(1));
   }
 
   private String translateGameMode(PlayerBattleLog playerBattleLog) {
@@ -118,7 +136,7 @@ public class DiscordMessageService {
     if ("challenge".equals(type)) {
       return playerBattleLog.getChallengeTitle();
     }
-    return playerBattleLog.getGameMode().getName() + " (" + playerBattleLog.getType() + ")";
+    return format(GAMEMODE_PATTERN, playerBattleLog.getGameMode().getName(), playerBattleLog.getType());
   }
 
 }
