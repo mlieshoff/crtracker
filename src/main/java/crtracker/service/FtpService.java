ac3141338f182a7841d18742729bfb3d31544445
mlieshoff <michaellieshoff@gmx.de>
2023-02-14 07:09:10 +0100

    Reduce complexity of FTP service

--- src/main/java/crtracker/service/FtpService.java
+++ src/main/java/crtracker/service/FtpService.java
@@ -1,5 +1,7 @@
 package crtracker.service;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import org.apache.commons.net.ftp.FTP;
 import org.apache.commons.net.ftp.FTPClient;
 import org.mili.utils.Lambda;
@@ -15,34 +17,38 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class FtpService extends BaseService {
 
-  public void upload(final String host, final int port, final String username, final String password, String filename,
-                     String content) throws ServiceException {
-    doInService(new Lambda<Void>() {
-      public Void exec(Object... params) throws Exception {
-        FTPClient ftpClient = new FTPClient();
-        try {
-          ftpClient.connect(host, port);
-          ftpClient.login(username, password);
-          ftpClient.enterLocalPassiveMode();
-          ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
-          InputStream inputStream = new ByteArrayInputStream(content.getBytes());
-          boolean done = ftpClient.storeFile(filename, inputStream);
-          inputStream.close();
-        } catch (IOException ex) {
-          ex.printStackTrace();
-        } finally {
-          try {
-            if (ftpClient.isConnected()) {
-              ftpClient.logout();
-              ftpClient.disconnect();
-            }
-          } catch (IOException ex) {
-            ex.printStackTrace();
-          }
-        }
-        return null;
-      }
+  public void upload(String host, int port, String username, String password, String filename, String content)
+      throws ServiceException {
+    doInService((Lambda<Void>) params -> {
+      uploadInternally(host, port, username, password, content, filename);
+      return null;
     });
+  }
+
+  private void uploadInternally(String host, int port, String username, String password, String content,
+                                String filename) {
+    FTPClient ftpClient = new FTPClient();
+    try {
+      ftpClient.connect(host, port);
+      ftpClient.login(username, password);
+      ftpClient.enterLocalPassiveMode();
+      ftpClient.setFileType(FTP.BINARY_FILE_TYPE);
+      InputStream inputStream = new ByteArrayInputStream(content.getBytes(UTF_8));
+      boolean done = ftpClient.storeFile(filename, inputStream);
+      log.debug("FTP upload of {} to {} result was: {}", filename, host, done);
+      inputStream.close();
+    } catch (IOException ex) {
+      log.warn("FTP upload failed", ex);
+    } finally {
+      try {
+        if (ftpClient.isConnected()) {
+          ftpClient.logout();
+          ftpClient.disconnect();
+        }
+      } catch (IOException ex) {
+        log.warn("Closing FTP client failed", ex);
+      }
+    }
   }
 
 }
